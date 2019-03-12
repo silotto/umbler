a8257f2c326a818ffd3508348f223a61223eef08
Silotto <silotto@tekzoom.com.br>
2019-03-12 20:20:15 -0300

    arquivo modificado

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplo Bobinho de Deploy Automatico na Umbler</title>
+</head>
+<body>
+<h1>Deu CERTO TEKER!!!!</h1>
 <?php
     phpinfo();
 ?>
+</body>
+</html>
+
+
